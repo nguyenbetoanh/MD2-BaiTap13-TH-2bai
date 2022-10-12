443d3da57f8074d9900b346e142f5ebbe1add47a
Nguyen Viet Anh <112063509+nguyenbetoanh@users.noreply.github.com>
2022-10-12 23:36:08 +0700

    2 bai

---
+++ TH/TH1/ThucHanh1/src/Main.java
@@ -0,0 +1,15 @@
+import static java.util.Arrays.binarySearch;
+
+public class Main {
+
+    private static int[] list;
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(list, 2));  /* 0 */
+        System.out.println(binarySearch(list, 11)); /* 4 */
+        System.out.println(binarySearch(list, 79)); /*12 */
+        System.out.println(binarySearch(list, 1));  /*-1 */
+        System.out.println(binarySearch(list, 5));  /*-1 */
+        System.out.println(binarySearch(list, 80)); /*-1 */
+    }
+}
